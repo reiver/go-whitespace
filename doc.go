3081d87036b0046de04ea570c7332632fb651397
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-12 09:15:09 -0700

    moved docs to its own file

---
+++ doc.go
@@ -0,0 +1,4 @@
+/*
+Package whitespace provides helper functions to deal with whitespace; and accounts for all 26 UNICODE whitespace characters.
+*/
+package whitespace
--- whitespace.go
+++ whitespace.go
@@ -1,4 +1,3 @@
-// Package whitespace provides helper functions to deal with whitespace; and accounts for all 26 UNICODE whitespace characters.
 package whitespace
 
 
